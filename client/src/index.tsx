c59dfd568c6a0d62bbf9c4337a825e47ea47d8b3
Jimmy <jimmylanbcn@gmail.com>
2021-12-18 18:19:06 -0500

    Add theme provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/system";
+import { theme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </CssBaseline>
   </React.StrictMode>,
   document.getElementById("root")
