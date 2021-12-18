3068c552197b3edcd86ace871ffbd30af49e99c5
Jimmy <jimmylanbcn@gmail.com>
2021-12-18 18:14:39 -0500

    Rename app

--- client/src/App.js
+++
@@ -1,5 +0,0 @@
-const App = () => {
-  return <div className="app-container"></div>;
-};
-
-export default App;
---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@mui/system";
+
+const App = () => {
+  return (
+    <Box height="100vh" width="100vw" bgcolor="">
+      Hi
+    </Box>
+  );
+};
+
+export default App;
