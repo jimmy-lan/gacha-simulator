fb40792852699dd00b090049a24d72e81c607e74
Jimmy <jimmylanbcn@gmail.com>
2021-12-18 23:02:05 -0500

    Add persistence class

---
+++ terminal/persistence.py
@@ -0,0 +1,19 @@
+import json
+from os import path
+from typing import Dict
+
+STORAGE_ROOT = "storage"
+
+
+class Persistence:
+    key: str
+
+    def __init__(self, key: str):
+        self.key = key
+
+    def get_path(self) -> str:
+        return f"{path.join(STORAGE_ROOT, self.key).strip('.')}.json"
+
+    def save_json(self, obj: Dict) -> None:
+        with open(self.get_path(), "w") as file:
+            json.dump(obj, file)
