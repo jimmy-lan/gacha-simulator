35684e0dce2abdb497066a3f9d6cf06519a1ee1d
Jimmy <jimmylanbcn@gmail.com>
2021-12-19 17:57:12 -0500

    Create random instance

--- terminal/controllers/banner.py
+++ terminal/controllers/banner.py
@@ -1,8 +1,7 @@
-import random
+from random import Random
 from typing import Dict, List
 
 from models.banner_reward import BannerReward
-from models.item_name import ItemName
 from models.reward_rarity import RewardRarity
 
 
@@ -10,9 +9,11 @@ class Banner:
     # A dictionary mapping reward rarity to an array of rewards.
     # A given reward may repeat multiple times in the list based on the weight of the reward.
     item_pool: Dict[RewardRarity, List[BannerReward]]
+    random: Random
 
     def __init__(self):
-        pass
+        self.item_pool = {}
+        self.random = Random()
 
     def add_reward(self, reward: BannerReward, rarity: RewardRarity = RewardRarity.common) -> None:
         if rarity not in self.item_pool:
@@ -22,7 +23,7 @@ class Banner:
     def shuffle(self, rarity: RewardRarity) -> None:
         if rarity not in self.item_pool:
             return
-        random.shuffle(self.item_pool[rarity])
+        self.random.shuffle(self.item_pool[rarity])
 
     def shuffle_all(self) -> None:
         for rarity in self.item_pool.keys():
