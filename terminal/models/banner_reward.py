ecef64ee94ae9695e697f416a5bb297ac2aec7ff
Jimmy <jimmylanbcn@gmail.com>
2021-12-19 17:14:52 -0500

    Add type declaration

--- terminal/models/banner_reward.py
+++ terminal/models/banner_reward.py
@@ -2,7 +2,11 @@ from models.item_name import ItemName
 
 
 class BannerReward:
+    name: str
+    quantity: int
+    weight: int
+
     def __init__(self, name: ItemName, quantity: int = 1, weight: int = 1):
-        self.name = name
+        self.name = name.value
         self.quantity = quantity
         self.weight = weight
