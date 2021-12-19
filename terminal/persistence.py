e6a1a0bfe1c793cbb0a7adc5ef685014a85896b0
Jimmy <jimmylanbcn@gmail.com>
2021-12-19 16:15:32 -0500

    Add docstring

--- terminal/persistence.py
+++ terminal/persistence.py
@@ -5,7 +5,9 @@ from typing import Dict
 
 
 class Persistence:
+    # Key that corresponds with the item being stored.
     key: str
+    # Root folder to persist this key.
     root: str
 
     def __init__(self, key: str, root: str = "storage"):
@@ -21,10 +23,17 @@ class Persistence:
         return f"{path.join(self.root, self.key).strip('.')}.json"
 
     def save_json(self, obj: Dict) -> None:
+        """
+        Save the given dictionary as json file.
+        :param obj: Dictionary to save.
+        """
         with open(self.get_path(), "w") as file:
             json.dump(obj, file)
 
     def read_json(self) -> Dict:
+        """
+        Read and return a dictionary corresponding to the current key.
+        """
         with open(self.get_path(), "r") as file:
             data = json.load(file)
         return data
