c3c5a5436ad79043d427d4d0b32099296bcc6601
Jimmy <jimmylanbcn@gmail.com>
2021-12-19 17:37:41 -0500

    Implement add_reward

--- terminal/controllers/banner.py
+++ terminal/controllers/banner.py
@@ -1,13 +1,19 @@
 from typing import Dict, List
 
+from models.banner_reward import BannerReward
 from models.item_name import ItemName
 from models.reward_rarity import RewardRarity
 
 
 class Banner:
-    # A dictionary mapping reward rarity to an array of items.
-    # An item may repeat multiple times in the list based on the weight of the item.
-    item_pool: Dict[RewardRarity, List[ItemName]]
+    # A dictionary mapping reward rarity to an array of rewards.
+    # A given reward may repeat multiple times in the list based on the weight of the reward.
+    item_pool: Dict[RewardRarity, List[BannerReward]]
 
     def __init__(self):
         pass
+
+    def add_reward(self, reward: BannerReward, rarity: RewardRarity = RewardRarity.common):
+        if rarity not in self.item_pool:
+            self.item_pool[rarity] = []
+        self.item_pool[rarity].extend([reward] * reward.weight)
