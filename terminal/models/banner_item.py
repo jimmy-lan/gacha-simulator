7a71b400d818e1b396bba157a525082ea01772ab
Jimmy <jimmylanbcn@gmail.com>
2021-12-19 16:57:01 -0500

    Add banner_item.py

---
+++ terminal/models/banner_item.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class BannerItem(Enum):
+    crystals = "Crystals"
+    daily_coins = "Daily Coins"
+    points = "Points"
+    commemorative_coins = "Commemorative Coins"
+    drink_vouchers = "Drink Vouchers"
+    drink_voucher_fragments = "Drink Voucher Fragments"
