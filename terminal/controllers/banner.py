f09f3f661d5b9b8339e5d2250b6f61daee2161ba
Jimmy <jimmylanbcn@gmail.com>
2021-12-19 17:28:00 -0500

    Declare item pool

--- terminal/controllers/banner.py
+++ terminal/controllers/banner.py
@@ -1,2 +1,13 @@
+from typing import Dict, List
+
+from models.item_name import ItemName
+from models.reward_rarity import RewardRarity
+
+
 class Banner:
+    # A dictionary mapping reward rarity to an array of items.
+    # An item may repeat multiple times in the list based on the weight of the item.
+    item_pool: Dict[RewardRarity, List[ItemName]]
+
     def __init__(self):
+        pass
