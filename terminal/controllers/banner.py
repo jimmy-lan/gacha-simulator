d7e0008506fc430880fc2946feefa0c26dcd1afa
Jimmy <jimmylanbcn@gmail.com>
2021-12-19 18:03:46 -0500

    Add rarity weights

--- terminal/controllers/banner.py
+++ terminal/controllers/banner.py
@@ -9,11 +9,16 @@ class Banner:
     # A dictionary mapping reward rarity to an array of rewards.
     # A given reward may repeat multiple times in the list based on the weight of the reward.
     item_pool: Dict[RewardRarity, List[BannerReward]]
+    # Weight of each rarity category.
+    rarity_weights: Dict[RewardRarity, float]
+    total_rarity_weight: float
     random: Random
 
-    def __init__(self):
+    def __init__(self, rarity_weights: Dict[RewardRarity, float]):
         self.item_pool = {}
         self.random = Random()
+        self.rarity_weights = rarity_weights
+        self.total_rarity_weight = sum(rarity_weights.values())
 
     def add_reward(self, reward: BannerReward, rarity: RewardRarity = RewardRarity.common) -> None:
         if rarity not in self.item_pool:
