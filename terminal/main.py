d6f7b2b6cfe82a3bef9a5853f1dcabd8dde068d3
Jimmy <jimmylanbcn@gmail.com>
2021-12-21 23:17:29 -0500

    Create examples

--- terminal/main.py
+++ terminal/main.py
@@ -5,17 +5,17 @@ from models.reward_rarity import RewardRarity
 
 if __name__ == '__main__':
     banner = Banner({
-        RewardRarity.common.value: 45,
-        RewardRarity.rare.value: 35,
-        RewardRarity.super_rare.value: 15,
-        RewardRarity.epic.value: 5
+        RewardRarity.common.value: 60,
+        RewardRarity.rare.value: 30,
+        RewardRarity.super_rare.value: 9.4,
+        RewardRarity.epic.value: 0.6
     })
     banner.pity_max_draw = {
-        RewardRarity.epic.value: 60,
+        RewardRarity.epic.value: 90,
         RewardRarity.super_rare.value: 10
     }
     banner.pity_threshold = {
-        RewardRarity.epic.value: 39,
+        RewardRarity.epic.value: 67,
         RewardRarity.super_rare.value: 8
     }
     banner.add_reward(BannerReward(ItemName.points, quantity=60, weight=80, rarity=RewardRarity.common.value))
@@ -26,5 +26,47 @@ if __name__ == '__main__':
     banner.add_reward(BannerReward(ItemName.points, quantity=500, weight=100, rarity=RewardRarity.super_rare.value))
     banner.add_reward(BannerReward(ItemName.points, quantity=2888, weight=100, rarity=RewardRarity.epic.value))
     banner.shuffle_all()
-    for i in range(10):
-        print(banner.wish())
+
+    count = {
+        RewardRarity.epic.value: 0,
+        RewardRarity.super_rare.value: 0,
+        RewardRarity.rare.value: 0,
+        RewardRarity.common.value: 0
+    }
+
+    while True:
+        sign = input(">>> ")
+        if sign == "q":
+            break
+        elif sign == "s":
+            print(count)
+            print(banner.pity_count)
+        elif sign == "v":
+            wish_res = banner.wish()
+            count[wish_res.rarity] += 1
+            print(wish_res)
+            print(f"Epic weight: {banner.get_weight(RewardRarity.epic.value)}, Super rare weight:" +
+                  f" {banner.get_weight(RewardRarity.super_rare.value)}, Total weight: {banner.total_rarity_weight}")
+
+        elif sign.isdigit():
+            for _ in range(int(sign)):
+                wish_res = banner.wish()
+                count[wish_res.rarity] += 1
+                print(wish_res)
+
+    # epic_count = 0
+    # num_epic_draws = 0
+    # num_total_draws = 1000000
+    # for _ in range(num_total_draws):
+    #     wish_res = banner.wish()
+    #     epic_count += 1
+    #     count[wish_res.rarity] += 1
+    #     if wish_res.rarity == RewardRarity.epic.value:
+    #         num_epic_draws += 1
+    #         if epic_count > 90:
+    #             exit()
+    #             print("ERR")
+    #         epic_count = 0
+    #
+    # print(count)
+    # print(f"avg: {num_total_draws / num_epic_draws}")
