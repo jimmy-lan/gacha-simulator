64bd0e75f8911760e27e304ca52d5571fb5334c6
Jimmy <jimmylanbcn@gmail.com>
2021-12-23 18:56:34 -0500

    Add save_banner helper

---
+++ terminal/helpers.py
@@ -0,0 +1,25 @@
+from constants.banner_rewards import BANNER_REWARDS
+from constants.banner_rules import RARITY_WEIGHTS, PITY_MAX_DRAW, PITY_THRESHOLD
+from controllers.banner import Banner
+from controllers.persistence import Persistence
+from models.banner_reward import BannerReward
+from models.storage_key import StorageKey
+
+
+def save_banner(banner: Banner) -> None:
+    pass
+
+
+def create_banner() -> Banner:
+    """
+    Initialize banner with constants and the current user statistics.
+    """
+    banner = Banner(RARITY_WEIGHTS)
+    banner.pity_max_draw = PITY_MAX_DRAW
+    banner.pity_threshold = PITY_THRESHOLD
+    for rarity in BANNER_REWARDS:
+        for item_name, quantity, weight in BANNER_REWARDS[rarity]:
+            reward = BannerReward(item_name, quantity=quantity, weight=weight, rarity=rarity)
+            banner.add_reward(reward)
+
+    return banner
