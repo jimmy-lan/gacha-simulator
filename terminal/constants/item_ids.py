6c905d3009781ccb2a9d2d3b4f6503ef683956da
Jimmy <jimmylanbcn@gmail.com>
2021-12-23 18:13:38 -0500

    Create item_ids.py

---
+++ terminal/constants/item_ids.py
@@ -0,0 +1,10 @@
+from models.item_name import ItemName
+
+ITEM_IDS = {
+    ItemName.points.value: "615a6b6d869d870014d6e0b2",
+    ItemName.crystals.value: "615a642ee697390013cae83b",
+    ItemName.daily_coins.value: "615a64a9e697390013cae83f",
+    ItemName.drink_vouchers.value: "61ca42f8869d870014d6e3a1",
+    ItemName.commemorative_coins.value: "61ca446c869d870014d6e3af",
+    ItemName.drink_voucher_fragments.value: "61ca4348869d870014d6e3a4"
+}
