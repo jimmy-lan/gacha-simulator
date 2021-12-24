299bbfe6e0be76aa112b14045dd569b4378546c8
Jimmy <jimmylanbcn@gmail.com>
2021-12-23 19:21:49 -0500

    Add wish_history property

--- terminal/models/user_stats.py
+++ terminal/models/user_stats.py
@@ -1,8 +1,10 @@
-from typing import Dict
+from typing import Dict, List
 
+from models.banner_reward import BannerReward
 from models.item_name import ItemName
 
 
 class UserStats:
     # Mapping item name to quantities.
     properties: Dict[ItemName, float]
+    wish_history: List[BannerReward]
