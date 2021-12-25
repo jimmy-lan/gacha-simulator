0564d8a6ebed69e469d21233b068ab2b45cbb4fb
Jimmy <jimmylanbcn@gmail.com>
2021-12-25 16:01:22 -0500

    Add command alias

--- terminal/commands/wish_command.py
+++ terminal/commands/wish_command.py
@@ -5,7 +5,7 @@ from models.command import Command
 
 class WishCommand(Command):
     def __int__(self):
-        super().__init__("wish", "Make wishes using wish wit.")
+        super().__init__("wish", "Make wishes using wish wit.", aliases=["r"])
 
     def execute(self):
         pass
