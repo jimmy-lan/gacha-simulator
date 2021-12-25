0a13fa0768439688613d27e2fd0a97a51b780afd
Jimmy <jimmylanbcn@gmail.com>
2021-12-25 15:13:58 -0500

    Implement set_subcommands method

--- terminal/commands/command_group.py
+++ terminal/commands/command_group.py
@@ -7,9 +7,14 @@ class CommandGroup(Command):
     subcommands: List[Command]
     _subcommand_map: Dict[str, Command]
 
-    def __init__(self, name: str, description: str, subcommands: List[Command]):
+    def __init__(self, name: str, description: str, subcommands: List[Command] = []):
         super().__init__(name, description)
+        self.set_subcommands(subcommands)
+
+    def set_subcommands(self, subcommands: List[Command]):
         self.subcommands = subcommands
+        self._build_subcommand_map()
+        self._validate_subcommands()
 
     def _build_subcommand_map(self):
         self._subcommand_map = {}
