fa5d3b4d2a5bd70eaf4999fa9796398f87b9060a
Jimmy <jimmylanbcn@gmail.com>
2021-12-25 16:54:53 -0500

    Add save methods

--- terminal/program_context.py
+++ terminal/program_context.py
@@ -1,5 +1,5 @@
 from controllers.banner import Banner
-from helpers import create_banner, read_user_stats
+from helpers import create_banner, read_user_stats, save_user_stats, save_banner
 from models.user_stats import UserStats
 
 
@@ -12,3 +12,11 @@ class ProgramContext:
         ProgramContext.banner = create_banner()
         ProgramContext.user_stats = UserStats()
         read_user_stats(ProgramContext.user_stats)
+
+    @staticmethod
+    def save_user_stats():
+        save_user_stats(ProgramContext.user_stats)
+
+    @staticmethod
+    def save_banner_pity():
+        save_banner(ProgramContext.banner)
