4e5fed1c888674934fc1952d8de5bebfa2b02487
Jimmy <jimmylanbcn@gmail.com>
2021-12-25 17:15:17 -0500

    Override execute command

--- terminal/commands/top_up_command.py
+++ terminal/commands/top_up_command.py
@@ -4,3 +4,6 @@ from models.command import Command
 class TopUpCommand(Command):
     def __init__(self):
         super().__init__("topup", "Top up items. Syntax: topup <points|wit> <amount>.")
+
+    def execute(self):
+        pass
