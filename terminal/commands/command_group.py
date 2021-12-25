df04ac8420b56ee0a65917fd899097607c85800f
Jimmy <jimmylanbcn@gmail.com>
2021-12-25 15:11:17 -0500

    Add _subcommand_map property

--- terminal/commands/command_group.py
+++ terminal/commands/command_group.py
@@ -1,11 +1,15 @@
-from typing import List
+from typing import List, Dict
 
 from commands.command import Command
 
 
 class CommandGroup(Command):
     subcommands: List[Command]
+    _subcommand_map: Dict[str, Command]
 
     def __init__(self, name: str, description: str, subcommands: List[Command]):
         super().__init__(name, description)
         self.subcommands = subcommands
+
+    def _build_subcommand_map(self):
+        pass
