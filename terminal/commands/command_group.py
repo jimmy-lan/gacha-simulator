6255d166e3dde25acd1b413e33d86e4bfdb63835
Jimmy <jimmylanbcn@gmail.com>
2021-12-25 15:10:36 -0500

    Accept subcommands property

--- terminal/commands/command_group.py
+++ terminal/commands/command_group.py
@@ -1,6 +1,11 @@
+from typing import List
+
 from commands.command import Command
 
 
 class CommandGroup(Command):
-    def __init__(self, name: str, description: str):
+    subcommands: List[Command]
+
+    def __init__(self, name: str, description: str, subcommands: List[Command]):
         super().__init__(name, description)
+        self.subcommands = subcommands
