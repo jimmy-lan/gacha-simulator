9b38d505fb19e37a05f85c4f183dfab04c447242
Jimmy <jimmylanbcn@gmail.com>
2021-12-26 11:42:20 -0500

    Access property from program context

--- terminal/commands/list_prop_command.py
+++ terminal/commands/list_prop_command.py
@@ -1,6 +1,10 @@
 from models.command import Command
+from program_context import ProgramContext
 
 
 class ListPropCommand(Command):
     def __init__(self):
         super().__init__("prop", "List user properties.", aliases=["p"])
+
+    def execute(self):
+        prop_map = ProgramContext.user_stats.properties
