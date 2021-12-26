5f62188405ccbd094828302f5a501e24eccb1de5
Jimmy <jimmylanbcn@gmail.com>
2021-12-26 11:55:09 -0500

    Implement pity list

--- terminal/commands/list_pity_command.py
+++ terminal/commands/list_pity_command.py
@@ -1,6 +1,13 @@
 from models.command import Command
+from program_context import ProgramContext
 
 
 class ListPityCommand(Command):
     def __init__(self):
         super().__init__("pity", "List number of wishes since last item with rarity.", aliases=["b"])
+
+    def execute(self):
+        pity_count = ProgramContext.banner.pity_count
+        print("--- [Pity Info] ---")
+        for rarity, count in pity_count.items():
+            print(f"- You have {count} wishes since your last {rarity} item.")
