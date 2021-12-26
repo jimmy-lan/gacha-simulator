9438d5c1f966748235f2507ecc39ca7a15bdaef9
Jimmy <jimmylanbcn@gmail.com>
2021-12-26 11:59:57 -0500

    Print redeem commands

--- terminal/commands/redeem_command.py
+++ terminal/commands/redeem_command.py
@@ -1,6 +1,15 @@
+from constants.item_ids import ITEM_IDS
 from models.command import Command
+from program_context import ProgramContext
 
 
 class RedeemCommand(Command):
     def __init__(self):
         super().__init__("redeem", "Redeem your rewards and clear your current inventory.")
+
+    def execute(self):
+        prop_map = ProgramContext.user_stats.properties
+        print("--- [Redeem Commands] ---")
+        for item_name, quantity in prop_map.items():
+            print(f"habits transaction create --property-id {ITEM_IDS[item_name]} -t \"[Redeem] Wish Rewards\" " +
+                  f"-a {quantity}")
