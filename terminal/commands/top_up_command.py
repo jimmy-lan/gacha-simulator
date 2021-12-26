9a0d52bf04781ee157b124d14caf994a20b88420
Jimmy <jimmylanbcn@gmail.com>
2021-12-26 11:24:57 -0500

    Implement execute method

--- terminal/commands/top_up_command.py
+++ terminal/commands/top_up_command.py
@@ -1,4 +1,6 @@
 from models.command import Command
+from models.item_name import ItemName
+from program_context import ProgramContext
 
 
 class TopUpCommand(Command):
@@ -15,8 +17,8 @@ class TopUpCommand(Command):
     def parse_arguments(self):
         if len(self.args) < 2:
             self._report_invalid_syntax()
-        self.top_up_item = self.args[0]
-        if self.top_up_item.lower() not in {"points", "wit"}:
+        self.top_up_item = self.args[0].lower()
+        if self.top_up_item not in {"points", "wit"}:
             self._report_invalid_syntax()
         if not self.args[1].isdigit():
             self._report_invalid_syntax()
@@ -26,3 +28,8 @@ class TopUpCommand(Command):
 
     def execute(self):
         self.parse_arguments()
+        if self.top_up_item == "points":
+            ProgramContext.user_stats.properties[ItemName.points.value] += self.amount
+        else:
+            ProgramContext.user_stats.wish_wit += self.amount
+        print(f"Top up completed: {self.top_up_item} * {self.amount}.")
