e03f0a9fda3871d556b9f6ae1f7665daf048ebf3
Jimmy <jimmylanbcn@gmail.com>
2021-12-26 12:39:57 -0500

    Update rare item color

--- terminal/constants/rarity_colors.py
+++ terminal/constants/rarity_colors.py
@@ -5,6 +5,6 @@ from models.reward_rarity import RewardRarity
 RARITY_COLORS = {
     RewardRarity.epic.value: Fore.YELLOW,
     RewardRarity.super_rare.value: Fore.MAGENTA,
-    RewardRarity.rare.value: Fore.CYAN,
+    RewardRarity.rare.value: Fore.BLUE,
     RewardRarity.common.value: Fore.GREEN
 }
