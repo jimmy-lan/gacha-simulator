15792ea07560d982ddd9abdee520a1ff662efc63
Jimmy <jimmylanbcn@gmail.com>
2021-12-27 16:38:38 -0500

    Show only epic and super rare pity stats

--- terminal/commands/pity_stats_command.py
+++ terminal/commands/pity_stats_command.py
@@ -1,4 +1,8 @@
+from colorama import Fore
+
+from constants.rarity_colors import RARITY_COLORS
 from models.command import Command
+from models.reward_rarity import RewardRarity
 from program_context import ProgramContext
 
 
@@ -8,6 +12,10 @@ class PityStatsCommand(Command):
 
     def execute(self):
         pity_count = ProgramContext.banner.pity_count
-        print("--- [Pity Info] ---")
-        for rarity, count in pity_count.items():
-            print(f"- You have {count} wishes since your last {rarity} item.")
+        epic_pity_count = pity_count[RewardRarity.epic.value]
+        super_rare_pity_count = pity_count[RewardRarity.super_rare.value]
+        print("--- [Pity Stats] ---")
+        print(f"- You have {epic_pity_count} wishes since your last " +
+              f"{RARITY_COLORS[RewardRarity.epic.value]}{RewardRarity.epic.value}{Fore.RESET} item.")
+        print(f"- You have {super_rare_pity_count} wishes since your last " +
+              f"{RARITY_COLORS[RewardRarity.super_rare.value]}{RewardRarity.super_rare.value}{Fore.RESET} item.")
