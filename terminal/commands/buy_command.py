ce277a09ee587bddb0aee1718446988a825256a2
Jimmy <jimmylanbcn@gmail.com>
2021-12-28 11:34:22 -0500

    Implement get_num_purchase

--- terminal/commands/buy_command.py
+++ terminal/commands/buy_command.py
@@ -6,14 +6,23 @@ from program_context import ProgramContext
 
 class BuyCommand(Command):
     def __init__(self):
-        super().__init__("buy", "Buy wish wits using points in your account.")
+        super().__init__(
+            "buy",
+            "Buy wish wits using points in your account." +
+            " Syntax: buy <num_wish_wits>."
+        )
 
     def _calc_max_num_purchase(self):
         points = ProgramContext.user_stats.properties[ItemName.points.value]
         return points // WISH_WIT_PRICE
 
     def num_purchase(self):
-        pass
+        if len(self.args) < 1:
+            return self._calc_max_num_purchase()
+        if not self.args[0].isdigit():
+            self.print_help()
+            raise Exception("Please enter a valid number of wish wits to purchase.")
+        return int(self.args[0])
 
     def execute(self):
         pass
