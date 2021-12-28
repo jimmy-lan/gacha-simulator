a99605059426fd5eb499ace9176af945027eb20f
Jimmy <jimmylanbcn@gmail.com>
2021-12-28 11:28:28 -0500

    Register name of command

--- terminal/commands/buy_command.py
+++ terminal/commands/buy_command.py
@@ -3,4 +3,4 @@ from models.command import Command
 
 class BuyCommand(Command):
     def __init__(self):
-        pass
+        super().__init__("buy", "Buy wish wits using points in your account.")
